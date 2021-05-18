bc90c941d6019204978cab9db2f2f60e4a4d9708
ry-15 <84328160+ry-15@users.noreply.github.com>
2021-05-18 10:59:05 +0800

    Create nine.java

---
+++ nine.java
@@ -0,0 +1,19 @@
+public class nine {
+
+    public static void main(String[] args) {
+
+        for (int j = 1; j <= 9; j++) {
+
+            for (int i = 1; i <= j; i++) {
+
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
